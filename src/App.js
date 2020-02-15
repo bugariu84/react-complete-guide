0045eac0ad07d68e643ca8e2353989c5268b9ec5
Bogdan Bugariu <bbugariu@protonmail.com>
2020-02-15 14:25:18 +0200

    node-modules integration

--- src/App.js
+++ src/App.js
@@ -6,34 +6,50 @@ class App extends Component {
   state = {
     index: 0,
     person: { name: "Bogdan", age: 35 },
+    showPersons: false,
     persons: [
-      { name: "Bogdan", age: 35 },
-      { name: "Catalin", age: 37 },
-      { name: "Mihai", age: 24 }
+      { name: "Bogdan", age: 35, id: 1 },
+      { name: "Catalin", age: 37, id: 2 },
+      { name: "Mihai", age: 24, id: 3 }
     ]
   }
 
-
-  switchNameHandler = () => {
-    let index = this.state.index + 1;
-
-    if (index === this.state.persons.length) {
-      index = 0;
+  style ={
+    button: {
+      padding: "10px",
+      backgroundColor: "green"
     }
+  };
 
-    this.setState({
-      index, 
-      person: this.state.persons[index]
-    });
+  togglePersonsHandler = (e) => {
+    this.setState({showPersons: !this.state.showPersons});
+  }
+
+  deletePersonHandler = (person, index) => {
+    let persons = [...this.state.persons];
+    persons = persons.slice(1, index);
+
+    this.setState({persons});
   }
 
   render() {
+    const persons = this.state.persons.map((person, index) => {
+      return (
+        <Person 
+          name={person.name} 
+          age={person.name} 
+          key={person.id} 
+          click={() => this.deletePersonHandler(index)}/>
+      );
+    });
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        {/* <Person name="Bogdan" age="35" >I like to feel myself</Person> */}
-        <Person name={this.state.person.name} age={this.state.person.age} />
+        <button style={this.style.button} onClick={this.togglePersonsHandler}>Toggle persons</button>
+
+        {this.state.showPersons ?
+          <div>{persons}</div> : ""
+        }
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
+
 const person = (props) => {
     return (
-        <div>
+        <div onClick={props.click}>
             <p>Hi, my name is <i>{props.name}</i> and i'm <i>{props.age}</i> years young!</p>
             {props.children ? <i>{props.children}</i> : ""}
         </div>
